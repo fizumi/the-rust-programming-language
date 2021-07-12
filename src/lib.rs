d0d668788446311b1e9f7bb2bd6bce8abc8ecc7d
fizumi <izumi.f.tech@gmail.com>
2021-07-12 06:32:19 +0000

    useキーワードでパスをスコープに持ち込む

--- src/lib.rs
+++ src/lib.rs
@@ -4,63 +4,14 @@ mod front_of_house {
 
         fn seat_at_table() {}
     }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        mod back_of_house {
-
-
-            fn fix_incorrect_order() {
-                cook_order();
-                super::serve_order(); // 相対パスの利用
-            }
-
-            fn cook_order() {}
-        }
-
-        fn take_payment() {}
-    }
 }
 
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String, // 一部のみ公開
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
-
-    pub enum Appetizer {
-        Soup,
-        Salad,
-    }
-}
+// use crate::front_of_house::hosting; // ✔OK
+// use self::front_of_house::hosting; // ✔OK
+use front_of_house::hosting; // ✔OK
+// use crate::front_of_house::hosting::add_to_waitlist; // ⚠ 文法は OK だが 慣例と異なる
 
 pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist(); // ← エラー解消
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist(); // ← エラー解消
-
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
-
-    meal.seasonal_fruit = String::from("blueberries"); // ← 構造体を公開してもフィールドは自動的に公開されない
-
-    // ↓ enumを公開すると、そのヴァリアントはすべて公開される。
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+    hosting::add_to_waitlist();
+    // add_to_waitlist(); // ⚠ 文法は OK だが 慣例と異なる
 }
