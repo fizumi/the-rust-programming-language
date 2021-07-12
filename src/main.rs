2fafba17783fd3deae71dc376bb159b86b0df448
fizumi <izumi.f.tech@gmail.com>
2021-07-12 05:40:00 +0000

    乱数を生成するコードの追加

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Guess the number!"); // std::println(https://doc.rust-lang.org/std/macro.println.html)
+    println!("Guess the number!"); // https://doc.rust-lang.org/std/macro.println.html
+
+    let secret_number = rand::thread_rng() // https://docs.rs/rand/0.8.4/rand/fn.thread_rng.html
+                                .gen_range(1..101); // https://docs.rs/rand/0.8.4/rand/trait.Rng.html#method.gen_range
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
-    let mut guess = String::new(); // std::string::String(https://doc.rust-lang.org/std/string/struct.String.html)/std::prelude(https://doc.rust-lang.org/std/prelude/index.html)
+    let mut guess = String::new(); // https://doc.rust-lang.org/std/string/struct.String.html
 
-    io::stdin()                         // std::io::stdin(https://doc.rust-lang.org/std/io/fn.stdin.html)
+    io::stdin()                         // https://doc.rust-lang.org/std/io/fn.stdin.html
         .read_line(&mut guess)          // https://doc.rust-lang.org/std/io/struct.Stdin.html#method.read_line
-        .expect("Failed to read line"); // std::io::Result（https://doc.rust-lang.org/std/io/type.Result.html）
+        .expect("Failed to read line"); // https://doc.rust-lang.org/std/io/type.Result.html
 
     println!("You guessed: {}", guess);
 }
