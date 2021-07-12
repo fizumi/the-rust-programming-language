ffc97a6806ce03d85da5a7033a9c81cb13ac5f12
fizumi <izumi.f.tech@gmail.com>
2021-07-12 06:35:40 +0000

    別ファイルのモジュールを使用する

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,8 @@
-mod front_of_house {
-    pub mod hosting { // 公開
-        pub fn add_to_waitlist() {} // 公開
+// ↓ Rustにモジュールの中身をモジュールと同じ名前をした別のファイルから読み込むように命令
+mod front_of_house;
 
-        fn seat_at_table() {}
-    }
-}
-
-// use crate::front_of_house::hosting; // ✔OK
-// use self::front_of_house::hosting; // ✔OK
-use front_of_house::hosting; // ✔OK
-// use crate::front_of_house::hosting::add_to_waitlist; // ⚠ 文法は OK だが 慣例と異なる
+pub use crate::front_of_house::hosting;
 
 pub fn eat_at_restaurant() {
     hosting::add_to_waitlist();
-    // add_to_waitlist(); // ⚠ 文法は OK だが 慣例と異なる
 }
